60050b963cec9cfea0177d144c91d29405d69daa
Sakshi Katore <sakshi.katore@gammaverse.in>
2025-07-19 08:30:35 +0530

    Completed JavaScript task

--- script.js
+++ script.js
@@ -7,21 +7,38 @@ let arr = [
 ];
 
 function PrintDeveloperbyMap() {
-  //Write your code here , just console.log
+  arr.map((employee) => {
+    if (employee.profession === "developer") {
+      console.log(employee);
+    }
+  });
 }
 
 function PrintDeveloperbyForEach() {
-  //Write your code here , just console.log
+  arr.forEach((employee) => {
+    if (employee.profession === "developer") {
+      console.log(employee);
+    }
+  });
 }
 
 function addData() {
-  //Write your code here, just console.log
+  let newEmployee = { id: 4, name: "susan", age: "20", profession: "intern" };
+  arr.push(newEmployee);
+  console.log(arr);
 }
 
 function removeAdmin() {
-  //Write your code here, just console.log
+  let filteredArr = arr.filter((employee) => employee.profession !== "admin");
+  console.log(filteredArr);
 }
 
 function concatenateArray() {
-  //Write your code here, just console.log
+  let newArr = [
+    { id: 5, name: "alice", age: "22", profession: "designer" },
+    { id: 6, name: "bob", age: "23", profession: "tester" },
+    { id: 7, name: "david", age: "24", profession: "product manager" },
+  ];
+  let combined = arr.concat(newArr);
+  console.log(combined);
 }
